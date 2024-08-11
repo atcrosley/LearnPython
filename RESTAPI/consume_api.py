b84eaf2ef0d309d6b246c94ae6c338c8345c18b9
atcrosley <atcrosley@gmail.com>
2024-08-11 16:37:07 -0400

    API practice

---
+++ RESTAPI/consume_api.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+response = requests.get('http://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow')
+
+print(response.json())
+
+for question in response.json()['items']:
+    print(question['title'])
+    print(question['link'])
+    print("\n")
